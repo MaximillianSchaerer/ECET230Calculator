8b79207ca88a6bc7f43aa150c0a46c6a87a7218a
Max Schaerer <MX1136@hotmail.com>
2024-09-30 13:53:41 -0700

    Added Calculator code

--- ECET230Calculator/Program.cs
+++ ECET230Calculator/Program.cs
@@ -1,20 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ECET230Calculator
+int num1  = 0;
+int num2 = 0;
+
+Console.WriteLine("Console Calculator in C#");
+Console.WriteLine("------------------------\n");
+
+Console.WriteLine("Enter the first integer number, then press Enter");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter the second integer number, then press Enter");
+num2  = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Choose an operation from the following list:");
+Console.WriteLine("\ta - Add");
+Console.WriteLine("\ts - Subtract");
+Console.WriteLine("\tm - Multiply");
+Console.WriteLine("\td - Divide");
+
+switch(Console.ReadLine())
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            int a = 42;
-            int b = 119;
-            int c = a + b;
-            Console.WriteLine(c);
-            Console.ReadKey();
-        }
-    }
+    case "a":
+        Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2));
+        break;
+
+    case "s":
+        Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
+        break;
+
+    case "m":
+        Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
+        break;
+
+    case "d":
+        Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
+        break;
 }
+
+Console.Write("Press any key to close the console");
+Console.ReadKey();
