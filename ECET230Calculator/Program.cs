25990488a7786682c3b72a7f008f31e3598d8505
Max Schaerer <MX1136@hotmail.com>
2024-09-30 13:07:45 -0700

    before deleting default code

--- ECET230Calculator/Program.cs
+++ ECET230Calculator/Program.cs
@@ -10,6 +10,11 @@ namespace ECET230Calculator
     {
         static void Main(string[] args)
         {
+            int a = 42;
+            int b = 119;
+            int c = a + b;
+            Console.WriteLine(c);
+            Console.ReadKey();
         }
     }
 }
